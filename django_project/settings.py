a69d7bed96552c38b2b1ac32f0475797288a59ee
Luis Carlos Mencia <lcmencia2@gmail.com>
2017-12-12 14:02:34 -0300

    django-material

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,6 +21,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
