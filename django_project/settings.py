2daca06f89b72864d65e31c2c0657aab043162a6
Lcmencia <lcmencia2@gmail.com>
2018-05-06 19:40:47 -0400

    Settings postgresql

--- django_project/settings.py
+++ django_project/settings.py
@@ -72,12 +72,23 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '30ce80e886e05f9621bc2c1b639d5a93',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
+
 
 
 # Internationalization
